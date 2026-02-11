91e3af5001ae98c3b38a0f73aabe6555be55ffde
Madhu-story <madhura25.rm@gmail.com>
2026-02-11 13:18:22 +0530

    Project 2, add some features

--- Project_2_Tip_Calculator.py
+++ Project_2_Tip_Calculator.py
@@ -9,10 +9,21 @@ print('Welcome to "The Grand"')
 bill = float(input("How much is the total bill? $"))
 split = int(input("How many people to split the bill? "))
 tip_choose = input("Would you like to give tip? Type 'Y' for yes and 'N' for no: ").capitalize()
-if tip_choose == "Y":
-    tip = int(input("How much tip would you like to add? 10, 12 or 15%? ")) 
+
+
+if tip_choose == "Y" and split != 0:
+    tip = int(input("How much tip would you like to add? 10, 12, 15 or customize the tip percentage? ")) 
     amount = (bill/split)*((tip/100)+1)
-else:
+
+elif tip_choose == "Y" and split == 0:
+    tip = int(input("How much tip would you like to add? 10, 12, 15 or customize the tip percentage? "))
+    amount = bill * ((tip/100)+1)  
+
+elif tip_choose == "N" and split != 0:
     amount = (bill/split)
+
+else:
+    amount = bill
+   
 amount = "{:.2f}".format(amount)
-print(f"Each person should pay ${amount}.")
+print(f"Each person pays ${amount}.")
