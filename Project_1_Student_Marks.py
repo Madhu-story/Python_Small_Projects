bd3d6ccd3fb650a6f45f4b4600870b97947b1f94
Madhu-story <madhura25.rm@gmail.com>
2026-02-16 13:29:06 +0530

    Level - 1, added functions in Project-1

--- Project_1_Student_Marks.py
+++ Project_1_Student_Marks.py
@@ -1,23 +1,36 @@
 print("----------------Student Marks Manager----------------")
 
-# Asking user to enter student's name:
-name = input("Enter Student's name: ")
 
+
+    
+# Asking user to enter student's name:
 # Asking user to enter 3 subjects marks that is obtained for the student:
+
+name = input("Enter Student's name: ")
 sub1 = int(input("Enter the marks obtained in Physics: "))
 sub2 = int(input("Enter the marks obtained in Chemistry: "))
 sub3 = int(input("Enter the marks obtained in Mathematics: "))
 
 # Adding all the 3 subject marks to get the total:
-Total = sub1 + sub2 + sub3
+def total():
+    Total = sub1 + sub2 + sub3
+    return Total
 
 # Taking average of the total marks of 3 subjects:
-average = Total/3
+def average():
+    total_marks = total()
+    Average = total_marks/3
+    Average = float('{:.2f}'.format(Average))
+    return Average
 
 # Checking if student has passed or fail:
-if (average >= 40):
-    print(name,  "Your score is", average)
-    print('"PASS"')
-else:
-    print(name,  "Your score is", average)
-    print('"FAIL"')
+def result():
+    average_marks = average()
+    if (average_marks >= 40):
+        print(name,  "Your score is", average_marks)
+        print('"PASS"')
+    else:
+        print(name,  "Your score is", average_marks)
+        print('"FAIL"')
+
+result()
